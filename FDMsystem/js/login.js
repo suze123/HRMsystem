8c470dad9294c5308eb108d3d4c9ccae3f814a33
suze <476028832@qq.com>
2016-04-19 15:17:18 +0800

    完善login登录界面，编写login.js，添加login.css 删除不必要文件夹rec

--- FDMsystem/js/login.js
+++ FDMsystem/js/login.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
+  //窗口竖直居中
+  var top=Math.round(($(window).height())/2-($("#d_login_bg").height())/2);
+  $("#d_login_bg").css("margin-top",top);
+  //登录提示
+var login_ts="请输入用户名和密码！^_^ ";
+  $("#login_ts").text(login_ts);
+  //重新获取验证码
+  $("#img_code").click(function(){
+    $("#img_code").attr("src", "../../resources/captcha.php"); 
+  });
+  //重置
+  $("#i_reset").click(function(){
+	$("#i_name").val("");
+	$("#i_password").val("");
+	$("#i_captcha").val("");
+	$("#i_name").focus();
+	$("#img_code").attr("src", "../../resources/captcha.php"); 
+  });
+  //登录
+   $("#i_submit").click(function(){
+	var zh=$("#i_name").val();
+	var pwd=$("#i_password").val();
+	var yzm=$("#i_captcha").val();
+	$.post("../php/login.php",{'zh':zh,'pwd':pwd,'yzm':yzm},function(result){
+    alert(result);
+	
+    });
 
-var l=Math.round(($(window).height())/2-($("#d_login_bg").height())/2);
-//var l=Math.round(($(window).height())/2);
-alert($("#d_login_bg").height());
-$("#d_login_bg").css("margin-top",l)
+  });
+
+  
 });
